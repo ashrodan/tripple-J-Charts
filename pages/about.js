99b6b937250be00bedbdb12b81fe6d6317067977
Ashley Rodan <8232214+ashrodan@users.noreply.github.com>
2019-11-08 20:48:30 +1100

    init nextjs boilerplate

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div>
+            <p> this is a the about page</p>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+        <Link href="/about" title="About Page">
+            <a>About page</a>
+        </Link>
+        <p> Hello World and Ash</p>
+    </div >
+)
+
+export default Index
