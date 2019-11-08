63c8dead4a5d3497c6d86d5f6a3b60c8446b3896
Ashley Rodan <8232214+ashrodan@users.noreply.github.com>
2019-11-08 21:25:07 +1100

    rebuild

---
+++ .next/static/uG5qnKrY_nLVrTO3dmI8p/pages/about.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{Juyh:function(n,t,u){"use strict";u.r(t),u.d(t,"default",function(){return o});var e=u("q1tI"),i=u.n(e).a.createElement;function o(){return i("div",null,i("p",null," this is a the about page"))}},rB5V:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u("Juyh")}])}},[["rB5V",1,0]]]);
